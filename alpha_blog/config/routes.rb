2fc83a6734db5490c658293936275b3d2d5644a6
Susie Rennick <susie.sko@gmail.com>
2022-01-01 15:21:14 -0800

    woopsie i broke it

--- alpha_blog/config/routes.rb
+++ alpha_blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  resources :articles
+  root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
 end
--- alpha_blog/db/schema.rb
+++ alpha_blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_224930) do
+ActiveRecord::Schema.define(version: 2022_01_01_231706) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
